9bd2194177f151ef1f6e991edae298c6c75e642a
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-13 22:51:25 -0800

    set up for matching users based on question

--- react-app/src/components/Discover/index.js
+++ react-app/src/components/Discover/index.js
@@ -10,26 +10,47 @@ const Discover = () => {
   const user_id = sessionUser?.user.id
   // console.log("user_id", user_id)
 
-  const questionObject = useSelector((state)=>state?.question)
+  const questionObject = useSelector((state)=>state.question)
   // console.log("questionObj", questionObject)
-  const questions = Object?.values(questionObject)
-  console.log("questions", questions)
+  const questions = Object.values(questionObject)
+  console.log("questions", questions[0])
 
 
 
 
   useEffect(()=>{
     dispatch(getQuestions())
-}, [dispatch, questions?.length])
+}, [dispatch, questions.length])
+
+// for each user's question object, we need to count how many answers
+// they have that are the same as the current user
+
+ const pointSystem = (user_id)
+
+
+ let currentUserQuestion = questions[0]?.filter((question) => {return question.user_id === user_id})
+ console.log("currentUserQuestion", currentUserQuestion)
+
+ let counter = {};
+
+  questions[0]?.map((question, ele) => {
+    console.log(ele, question)
+    // console.log("question.question1", question.question1)
+    // console.log("currentUserQuestion.question1", currentUserQuestion[0].question1)
+    if(!counter[question.user_id]) {
+      counter[question.user_id] = 0
+    }
+    if(question.question1 === currentUserQuestion[0].question1) {
+      counter[question.user_id] += 1
+    }
+
+  })
+  console.log("count", counter)
 
 
 
-  // questions.map((question) => {
-  //   console.log("hi", question)
-  // })
-  console.log("id----", questions[0])
-  let userQuestion = questions[0]
-  // console.log("userQuestion[0]", userQuestion[0])
+  // console.log("id----", questions[0])
+  // let userQuestion = questions[0]
 
 
   return (
