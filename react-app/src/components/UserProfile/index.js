6eeb15edd87ce4880678c5c438edddbffaac6625
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-14 08:52:49 -0800

    able to display information in current user profile

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { getProfile } from "../../store/profile";
+
 
 function UserProfile() {
   const [user, setUser] = useState({});
   const { userId }  = useParams();
+  const dispatch = useDispatch()
+  let profileObj = useSelector((state) => state?.profile[userId])
 
+
+// get current user
   useEffect(() => {
     if (!userId) {
       return;
@@ -16,13 +23,23 @@ function UserProfile() {
     })();
   }, [userId]);
 
+// get one profile
+  useEffect(() => {
+    dispatch(getProfile(userId));
+  }, [dispatch, userId]);
+
+
   if (!user) {
     return null;
   }
 
+
   return (
     <>
     <div> in the userprofile component</div>
+    <div>
+      {profileObj?.goal}
+    </div>
     <ul>
       <li>
         <strong>User Id</strong> {userId}
