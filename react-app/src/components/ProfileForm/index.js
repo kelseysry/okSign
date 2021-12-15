daddee94d4a93371174e50c052934e14bcd75d58
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-14 22:39:40 -0800

    created backend and thunk for create profile

---
+++ react-app/src/components/ProfileForm/index.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+import { useDispatch} from 'react-redux';
+import { useSelector } from "react-redux";
+import {useParams} from 'react-router-dom';
+import { createProfile } from "../../store/profile";
+
+const ProfileForm = () => {
+
+
+
+}
