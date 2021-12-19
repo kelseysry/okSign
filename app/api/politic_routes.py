6a314672a30d19779f8cb74935893d7e585ede0c
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-18 21:49:12 -0800

    prep politic route

---
+++ app/api/politic_routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify, redirect, url_for, session, request
+from app.models import db, User, Politic
+
+
+drinking_routes = Blueprint('drinkings', __name__)
+
+# get all Drinking in db
+@drinking_routes.route('/')
+def drinkings():
+    drinkings = Drinking.query.all()
+    return {'drinkings': [drinking.to_dict() for drinking in drinkings]}
