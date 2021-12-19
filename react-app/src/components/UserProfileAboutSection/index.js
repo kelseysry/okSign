e7f47bba7bc73f40098f52c6f05103f4a9e93648
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-18 19:45:29 -0800

    clean up about me

--- react-app/src/components/UserProfileAboutSection/index.js
+++ react-app/src/components/UserProfileAboutSection/index.js
@@ -5,7 +5,7 @@ import { getHoroscopes } from '../../store/horoscope';
 function UserProfileAboutSection(currentUserProfile) {
   const dispatch = useDispatch()
 
-  console.log("currentProfile", currentUserProfile)
+  // console.log("currentProfile", currentUserProfile)
 
   let currentProfile = currentUserProfile.currentUserProfile
 
@@ -28,97 +28,95 @@ function UserProfileAboutSection(currentUserProfile) {
     }
   }
 
-
   return (
-<>
+    <>
+        <div>
+          about me : {currentProfile[0]?.about_me}
+        </div>
+        <div>
+          goal : {currentProfile[0]?.goal}
+        </div>
+        <div>
+          talent : {currentProfile[0]?.talent}
+        </div>
+        <div>
+          my traits : {currentProfile[0]?.my_traits}
+        </div>
+        <div>
+          needs : {currentProfile[0]?.needs}
+        </div>
+        <div>
+          hobbies : {currentProfile[0]?.hobbies}
+        </div>
+        <div>
+          moments : {currentProfile[0]?.moments}
+        </div>
+        <div>
+          secrets : {currentProfile[0]?.secrets}
+        </div>
+        <div>
+          looking for : {currentProfile[0]?.looking_for}
+        </div>
+        <div>
+          user audio : {currentProfile[0]?.user_audio}
+        </div>
 
-    <div>
-    about me : {currentProfile[0]?.about_me}
-  </div>
-  <div>
-    goal : {currentProfile[0]?.goal}
-  </div>
-  <div>
-    talent : {currentProfile[0]?.talent}
-  </div>
-  <div>
-    my traits : {currentProfile[0]?.my_traits}
-  </div>
-  <div>
-    needs : {currentProfile[0]?.needs}
-  </div>
-  <div>
-    hobbies : {currentProfile[0]?.hobbies}
-  </div>
-  <div>
-    moments : {currentProfile[0]?.moments}
-  </div>
-  <div>
-    secrets : {currentProfile[0]?.secrets}
-  </div>
-  <div>
-    looking for : {currentProfile[0]?.looking_for}
-  </div>
-  <div>
-    user audio : {currentProfile[0]?.user_audio}
-  </div>
+        <section className="Details">
+          <h1> Details </h1>
+          <div>
+            gender : {currentProfile[0]?.gender_id}
+          </div>
+          <div>
+            number of likes: {currentProfile[0]?.number_likes}
+          </div>
+          <div>
+            orientation : {currentProfile[0]?.orientation_id}
+          </div>
+          <div>
+            pronouns: {currentProfile[0]?.pronouns}
+          </div>
+          <div>
+            height: {currentProfile[0]?.height} cm
+          </div>
+          <div>
+            education: {currentProfile[0]?.education}
+          </div>
+          <div>
+            occupation: {currentProfile[0]?.occupation}
+          </div>
+          <div>
+            horoscope : {getHoroscope(currentProfile[0]?.horoscope_id)}
+          </div>
+          <div>
+            smoking_id: {currentProfile[0]?.smoking_id}
+          </div>
+          <div>
+            drinking_id: {currentProfile[0]?.drinking_id}
+          </div>
 
-  <section className="Details">
-    <h1> Details </h1>
-    <div>
-      gender : {currentProfile[0]?.gender_id}
-    </div>
-    <div>
-      number of likes: {currentProfile[0]?.number_likes}
-    </div>
-    <div>
-      orientation : {currentProfile[0]?.orientation_id}
-    </div>
-    <div>
-      pronouns: {currentProfile[0]?.pronouns}
-    </div>
-    <div>
-      height: {currentProfile[0]?.height} cm
-    </div>
-    <div>
-      education: {currentProfile[0]?.education}
-    </div>
-    <div>
-      occupation: {currentProfile[0]?.occupation}
-    </div>
-    <div>
-      horoscope : {getHoroscope(currentProfile[0]?.horoscope_id)}
-    </div>
-    <div>
-      smoking_id: {currentProfile[0]?.smoking_id}
-    </div>
-    <div>
-      drinking_id: {currentProfile[0]?.drinking_id}
-    </div>
-
-    {/* <div>
-      smoking: {currentProfile[0]?.smoking? "Smokes" : "Doesn't smoke"}
-    </div> */}
-    {/* <div>
-      <i className="fas fa-cocktail"></i> {currentProfile[0]?.smoking? "Drinks" : "Doesn't drink"}
-    </div> */}
-    <div>
-      children_id: {currentProfile[0]?.children_id}
-    </div>
-    <div>
-      pet_id: {currentProfile[0]?.pet_id}
-    </div>
-    <div>
-      politic_id: {currentProfile[0]?.politic_id}
-    </div>
-    <div>
-      religion_id: {currentProfile[0]?.religion_id}
-    </div>
-    <div>
-      partner_id: {currentProfile[0]?.partner_id}
-    </div>
-  </section>
-  </>
+          {/* <div>
+            smoking: {currentProfile[0]?.smoking? "Smokes" : "Doesn't smoke"}
+          </div> */}
+          {/* <div>
+            <i className="fas fa-cocktail"></i> {currentProfile[0]?.smoking? "Drinks" : "Doesn't drink"}
+          </div> */}
+          <div>
+            children_id: {currentProfile[0]?.children_id}
+          </div>
+          <div>
+            pet_id: {currentProfile[0]?.pet_id}
+          </div>
+          <div>
+            politic_id: {currentProfile[0]?.politic_id}
+          </div>
+          <div>
+            religion_id: {currentProfile[0]?.religion_id}
+          </div>
+          <div>
+            partner_id: {currentProfile[0]?.partner_id}
+          </div>
+        </section>
+      </>
   )
 }
 
