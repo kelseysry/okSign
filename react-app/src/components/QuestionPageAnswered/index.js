319e32e0be7f3dce18264325bec339cc0c8f727f
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-19 21:25:45 -0800

    take out useState

--- react-app/src/components/QuestionPageAnswered/index.js
+++ react-app/src/components/QuestionPageAnswered/index.js
@@ -11,36 +11,11 @@ function QuestionPageAnswered() {
   const questionObject = useSelector((state)=>state.question)
   const questions = Object.values(questionObject)
 
-  const [loaded, setLoaded] = useState(false)
 
+  useEffect(() => {
+   dispatch(getQuestion(user_id))
 
-  const [question1, setQuestion1] = useState('');
-  const [must_answer1, setMust_answer1] = useState('');
-  const [question2, setQuestion2] = useState('');
-  const [must_answer2, setMust_answer2] = useState('');
-  const [question3, setQuestion3] = useState('');
-  const [must_answer3, setMust_answer3] = useState('');
-  const [question4, setQuestion4] = useState('');
-  const [must_answer4, setMust_answer4] = useState('');
-  const [question5, setQuestion5] = useState('');
-  const [must_answer5, setMust_answer5] = useState('');
-  const [question6, setQuestion6] = useState('');
-  const [must_answer6, setMust_answer6] = useState('');
-  const [question7, setQuestion7] = useState('');
-  const [must_answer7, setMust_answer7] = useState('');
-  const [question8, setQuestion8] = useState('');
-  const [must_answer8, setMust_answer8] = useState('');
-  const [question9, setQuestion9] = useState('');
-  const [must_answer9, setMust_answer9] = useState('');
-  const [question10, setQuestion10] = useState('');
-  const [must_answer10, setMust_answer10] = useState('');
-
-  useEffect(async() => {
-   let questionLoaded = await dispatch(getQuestion(user_id))
-   if(questionLoaded) {
-    setLoaded(true)
-   }
-  },[dispatch, user_id, loaded])
+  },[dispatch, user_id])
 
   // console.log("user_id", user_id)
   console.log("questions", questions[0]?.id)
