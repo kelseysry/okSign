52d610a7f7f622dc879a4531c25736a0b9f70af9
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-19 21:38:10 -0800

    question 4

--- react-app/src/components/QuestionPageAnswered/index.js
+++ react-app/src/components/QuestionPageAnswered/index.js
@@ -87,6 +87,67 @@ function QuestionPageAnswered() {
         </section>
 
 
+        <section className="question3Container">
+          <div className="questionText">3. If you don't do anything at all for an entire day, how does that make you feel?</div>
+          <div className ="question">
+            <RadioButton
+                label="Good"
+                value={'Good' === questions[0]?.question3}
+                onClick={false}
+              />
+            <RadioButton
+              label="Bad"
+              value={'Bad' === questions[0]?.question3}
+              onClick={false}
+            />
+          </div>
+
+          <div className="answer">Answer you'll accept?</div>
+          <div className ="question">
+            <RadioButton
+                label="Good"
+                value={'Good' === questions[0]?.must_answer3}
+                onClick={false}
+              />
+            <RadioButton
+              label="Bad"
+              value={'Bad' === questions[0]?.must_answer3}
+              onClick={false}
+            />
+          </div>
+        </section>
+
+        <section className="question4Container">
+          <div className="questionText">4. Do you often find yourself worrying about things that you have no control over?</div>
+          <div className ="question">
+            <RadioButton
+                label="Yes"
+                value={'Yes' === questions[0]?.question4}
+                onClick={false}
+              />
+            <RadioButton
+              label="No"
+              value={'No' === questions[0]?.question4}
+              onClick={false}
+            />
+          </div>
+
+          <div className="answer">Answer you'll accept?</div>
+          <div className ="question">
+            <RadioButton
+                label="Yes"
+                value={'Yes' === questions[0]?.must_answer4}
+                onClick={false}
+              />
+            <RadioButton
+              label="No"
+              value={'No' === questions[0]?.must_answer4}
+              onClick={false}
+            />
+          </div>
+        </section>
+
+
     </div>
 
 
