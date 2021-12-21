22e33c85b2da7aea1f4af48af4a70efccf469bd7
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-20 22:14:57 -0800

    styled create profile form

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -164,9 +164,15 @@ function UserProfile({count, setCount}) {
     <>
 
        {   currentProfile?.length ? content_edit_compiled :
-         <div>
-           <NavLink to={`/createProfile`}><div className=""></div>Create Profile <i className="fas fa-address-card"></i></NavLink>
-        </div>
+          <>
+            <div className="questionAnswered">
+              <div className="questionsHeader">Create Your Profile!</div>
+            </div>
+            
+            <div>
+              <NavLink to={`/createProfile`}><div className=""></div>Create Profile <i className="fas fa-address-card"></i></NavLink>
+            </div>
+          </>
        }
 
     </>
