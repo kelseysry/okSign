42d130dff22bc48e662e6a2e305933ec51a6e8c5
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-24 23:08:12 -0800

    pic scrollable

--- react-app/src/components/MatchProfilePics/index.js
+++ react-app/src/components/MatchProfilePics/index.js
@@ -146,15 +146,17 @@ const MatchProfilePics = ({matchProfileObj}) => {
 
           </div>
       <div className="oneMatchProfileContainerPage">
-
+        <div className="pic-container-scroll">
           <div className="match_profile_images_container_user">
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url1} alt="match_image"/>
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url2} alt="match_image"/>
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url3} alt="match_image"/>
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url4} alt="match_image"/>
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url5} alt="match_image"/>
-            <img className="match_profile_image_discover" src={matchProfileObj[0]?.image_url6} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url1} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url2} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url3} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url4} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url5} alt="match_image"/>
+            <img className="match_profile_image_discover_page" src={matchProfileObj[0]?.image_url6} alt="match_image"/>
           </div>
+
+        </div>
       </div>
       </>
 
