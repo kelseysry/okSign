b70bf6a17af3f3fe79088ab1e91a825f6f3323ec
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-26 12:19:35 -0800

    quick cosmetic change, heart

--- react-app/src/components/Discover/ChooseDiscoverContent.js
+++ react-app/src/components/Discover/ChooseDiscoverContent.js
@@ -7,11 +7,11 @@ function ChooseDiscoverContent() {
   return (
     <div className='discoverContent-buttons-container'>
       <div className="option">
-       {discoverContent === 'QuestionMatch' ? <div className="heart-choice">💕</div> : <div>&nbsp;</div>}
+       {discoverContent === 'QuestionMatch' ? <div className="heart-choice">💖</div> : <div>&nbsp;</div>}
         <button className="QM" onClick={() => setDiscoverContent('QuestionMatch')}>Question Match</button>
       </div>
       <div className="option">
-      {discoverContent === 'HoroscopeMatch' ? <div className="heart-choice">💕</div> : <div>&nbsp;</div>}
+      {discoverContent === 'HoroscopeMatch' ? <div className="heart-choice">💖</div> : <div>&nbsp;</div>}
         <button className="HM" onClick={()=> setDiscoverContent('HoroscopeMatch')}>Horoscope Match</button>
       </div>
     </div>
