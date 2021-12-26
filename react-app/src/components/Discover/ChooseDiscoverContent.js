5dd05f4315f8a625dd9e47c32793df6410d86aee
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-26 12:16:29 -0800

    add heart

--- react-app/src/components/Discover/ChooseDiscoverContent.js
+++ react-app/src/components/Discover/ChooseDiscoverContent.js
@@ -6,12 +6,12 @@ function ChooseDiscoverContent() {
 
   return (
     <div className='discoverContent-buttons-container'>
-      <div className="option1">
-       {discoverContent === 'QuestionMatch' ? <div>💖</div> : <div>&nbsp;</div>}
+      <div className="option">
+       {discoverContent === 'QuestionMatch' ? <div className="heart-choice">💕</div> : <div>&nbsp;</div>}
         <button className="QM" onClick={() => setDiscoverContent('QuestionMatch')}>Question Match</button>
       </div>
-      <div className="option1">
-      {discoverContent === 'HoroscopeMatch' ? <div>💖</div> : <div>&nbsp;</div>}
+      <div className="option">
+      {discoverContent === 'HoroscopeMatch' ? <div className="heart-choice">💕</div> : <div>&nbsp;</div>}
         <button className="HM" onClick={()=> setDiscoverContent('HoroscopeMatch')}>Horoscope Match</button>
       </div>
     </div>
