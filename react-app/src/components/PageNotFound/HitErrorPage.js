b296147e7435c11c455fce0b1fefc5214df84239
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-29 10:53:12 -0800

    weird white space at bottom

--- react-app/src/components/PageNotFound/HitErrorPage.js
+++ react-app/src/components/PageNotFound/HitErrorPage.js
@@ -7,28 +7,34 @@ import {errorMessage as allMarkers} from '../../data/errorMessage'
 function HitErrorPage () {
 
   return (
-    <>
 
-<div className="container-banner">
-      <div className="top-banner-pic" style={{ backgroundImage: `url('${pictures.collection[0].imageUrl}')` }}>
+  <div style={{ backgroundImage: `url('${pictures.collection[0].imageUrl}')` }}>
+
+
+
+      <div className="container-banner">
+      <div className="top-banner-pic">
         <div className="header-banner-text">oh no</div>
         <div className="header-banner-sign">404</div>
 
         <div className="text-banner">Please navigate back to home,</div>
         <div className="text-banner">you won't find any matches here!</div>
+
+
+
       </div>
     </div>
-
-
     <div className="error-map-container">
       <ErrorMapContainer allMarkers={allMarkers} />
     </div>
 
+    </div>
 
 
 
 
-    </>
+
+
   )
 }
 
