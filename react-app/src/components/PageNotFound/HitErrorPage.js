8e2429fe99be50f39444970be4f2974f9c130b75
Kelsey Sry <ksry@alumni.stanford.edu>
2021-12-29 10:36:05 -0800

    style 404 head banner to glow

--- react-app/src/components/PageNotFound/HitErrorPage.js
+++ react-app/src/components/PageNotFound/HitErrorPage.js
@@ -8,9 +8,17 @@ function HitErrorPage () {
 
   return (
     <>
-    <div className="error-pic-container" style={{ backgroundImage: `url('${pictures.collection[0].imageUrl}')` }}>
-      <div className="error-font">404</div>
+
+<div className="container-banner">
+      <div className="top-banner-pic" style={{ backgroundImage: `url('${pictures.collection[0].imageUrl}')` }}>
+        <div className="header-banner-text">oh no</div>
+        <div className="header-banner-sign">404</div>
+
+        <div className="text-banner">Please navigate back to home,</div>
+        <div className="text-banner">you won't find any matches here!</div>
+      </div>
     </div>
+
 
     <div className="error-map-container">
       <ErrorMapContainer allMarkers={allMarkers} />
