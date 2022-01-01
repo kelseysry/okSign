7523916eb5788656741b0b9da771f12483ebe8d7
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-01 12:32:46 -0800

    change text format of no matches

--- react-app/src/components/NoMatches/index.js
+++ react-app/src/components/NoMatches/index.js
@@ -2,11 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import React, { useState, useEffect } from 'react';
 import { getProfiles } from '../../store/profile';
 import { useHistory } from 'react-router';
+import { useDiscoverContent } from "../../context/DiscoverContentContext";
 
 import './NoMatches.css'
 
 const NoMatches = ({user_id}) => {
   const dispatch = useDispatch()
+  const {discoverContent, setDiscoverContent} = useDiscoverContent()
 
   const profilesObj = useSelector((state) => state?.profile)
   const profiles = Object?.values(profilesObj)[0]
@@ -34,10 +36,36 @@ const NoMatches = ({user_id}) => {
 
   return (
     <>
+
+
+
+
+<section className="step-container">
+          <div className="Step1N">No Matches</div>
+          <div className="Step2N">yet...</div>
+          <div className="Step3N">answer</div>
+          <div className="Step3NQ">questions</div>
+
+          {/* <button id={discoverContent === 'HoroscopeMatch' ? 'whiteFont' : 'orangeFont'} className="Step4" onClick={() => setDiscoverContent('QuestionMatch')}>Questions</button>
+          <div className={discoverContent === 'HoroscopeMatch' ? 'StepClick1' : 'hideClickMe' }>Click Me</div>
+
+
+          <div className="Step5">Or</div>
+          <button id={discoverContent === 'HoroscopeMatch' ? 'orangeFont' : 'whiteFont'} className="Step6" onClick={()=> setDiscoverContent('HoroscopeMatch')}>Horoscope</button>
+          <div className={discoverContent === 'HoroscopeMatch' ? 'hideClickMe' : 'StepClick2' }>Click Me</div> */}
+        </section>
+
+
       <section className="NoMatchesContainer">
         <div className="no-matches-header-container">
              <div className="no-matches-header">
                   No Matches Yet!
+
+
+
+
+
+
               </div>
         </div>
       </section>
