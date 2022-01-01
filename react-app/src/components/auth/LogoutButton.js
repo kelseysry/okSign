4908cfce5bde5dfc14b6529643dcf2ee67535aaf
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-01 00:02:13 -0800

    close navbar when logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { clearQuestions } from '../../store/question';
 import { logout } from '../../store/session';
+import {closeNav} from '../../store/navigation'
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
     await dispatch(clearQuestions())
+    await dispatch(closeNav())
   };
 
   return <button className="" onClick={onLogout}>
