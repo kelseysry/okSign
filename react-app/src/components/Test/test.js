0a04f265ed56ba439fb3f58ebb376eeb0b39042c
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-01 19:27:05 -0800

    question 2 style format

--- react-app/src/components/Test/test.js
+++ react-app/src/components/Test/test.js
@@ -43,44 +43,88 @@ return (
   <a href="#question-10">10</a>
 </nav>
 
-  <div class="scroll-container">
-      <div class="scroll-question1-section" id="question-1">
-        <div className="question-big"> 1. Which word describes you better?</div>
-        <div className="vertical-line"></div>
+    <section class="scroll-container">
 
-      <section className="question1ContainerE">
-              <div className="questionTextE">Your Answer?</div>
-              <div className ="question-choice">
-                <RadioButton
-                    label="Carefree"
-                    value={question1 === 'Carefree'}
-                    onChange={(e) => setQuestion1("Carefree")}
-                  />
-                <RadioButton
-                  label="Intense"
-                  value={question1 === 'Intense'}
-                  onChange={(e) => setQuestion1("Intense")}
-                />
-              </div>
+        <div class="scroll-question1-section" id="question-1">
+          <div className="question-big"> 1. Which word describes you better?</div>
+          <div className="vertical-line"></div>
+            <section className="question1ContainerE">
+                    <div className="questionTextE">Your Answer?</div>
+                    <div className ="question-choice">
+                      <RadioButton
+                          label="Carefree"
+                          value={question1 === 'Carefree'}
+                          onChange={(e) => setQuestion1("Carefree")}
+                        />
+                      <RadioButton
+                        label="Intense"
+                        value={question1 === 'Intense'}
+                        onChange={(e) => setQuestion1("Intense")}
+                      />
+                    </div>
 
-              <div className="questionTextE">Answer you'll accept?</div>
-              <div className ="question-choice">
-                <RadioButton
-                    label="Carefree"
-                    value={must_answer1 === 'Carefree'}
-                    onChange={(e) => setMust_answer1("Carefree")}
-                  />
-                <RadioButton
-                  label="Intense"
-                  value={must_answer1 === 'Intense'}
-                  onChange={(e) => setMust_answer1("Intense")}
-                />
-              </div>
-        </section>
-          <div className="horizontal-line"></div>
-    </div>
+                    <div className="questionTextE">Answer you'll accept?</div>
+                    <div className ="question-choice">
+                      <RadioButton
+                          label="Carefree"
+                          value={must_answer1 === 'Carefree'}
+                          onChange={(e) => setMust_answer1("Carefree")}
+                        />
+                      <RadioButton
+                        label="Intense"
+                        value={must_answer1 === 'Intense'}
+                        onChange={(e) => setMust_answer1("Intense")}
+                      />
+                    </div>
+              </section>
+            <div className="horizontal-line"></div>
+        </div>
 
-  </div>
+
+
+
+        <div class="scroll-question2-section" id="question-2">
+          <div className="question-big">2. Choose the better romantic activity</div>
+          <div className="vertical-line"></div>
+            <section className="question1ContainerE">
+                    <div className="questionTextE">Your Answer?</div>
+                    <div className ="question-choice">
+                    <RadioButton
+                        label="Kissing in Paris"
+                        value={question2 === 'Kissing in Paris'}
+                        onChange={(e) => setQuestion2("Kissing in Paris")}
+                      />
+                    <RadioButton
+                        label="Kissing in a tent, in the woods"
+                        value={question2 === 'Kissing in a tent, in the woods'}
+                        onChange={(e) => setQuestion2("Kissing in a tent, in the woods")}
+                      />
+                    </div>
+
+                    <div className="questionTextE">Answer you'll accept?</div>
+                    <div className ="question-choice">
+                      <RadioButton
+                          label="Carefree"
+                          value={must_answer1 === 'Carefree'}
+                          onChange={(e) => setMust_answer1("Carefree")}
+                        />
+                      <RadioButton
+                        label="Kissing in a tent, in the woods"
+                        value={must_answer2 === "Kissing in a tent, in the woods"}
+                        onChange={(e) => setMust_answer2("Kissing in a tent, in the woods")}
+                      />
+                    </div>
+              </section>
+            <div className="horizontal-line"></div>
+        </div>
+
+
+
+
+
+
+
+    </section>
 
 
 </section>
