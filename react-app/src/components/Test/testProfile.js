ed1261d2ebaca819cba7dd554850d7ed9fc8afd4
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-01 21:07:53 -0800

    template grid for profile form

--- react-app/src/components/Test/testProfile.js
+++ react-app/src/components/Test/testProfile.js
@@ -9,7 +9,7 @@ import RadioButton from '../RadioButton';
 import { createQuestion } from "../../store/question"
 import { useHistory } from 'react-router';
 
-
+import './TestProfile.css'
 
 
 function TestProfile() {
@@ -26,58 +26,102 @@ function TestProfile() {
   const [must_answer3, setMust_answer3] = useState('');
 
 
+  /*
+
+  1. person emoji
+  - age
+  - gender - select
+  - pronouns
+  - orientation - select
+  - height
+
+
+  2. map emoji
+  location
+  lat
+  lng
+
+  3. book
+  - about me
+  - hobbies
+  - education
+  - occupation
+
+  4. trophy
+  - goal
+  - talent
+  - traits
+
+
+  4. Relationship
+  - relationship status
+  - needs
+  - looking for
+  - gender preference
+
+  5. Funny
+  - moments
+  - secrets
+
+  6. Shower thought
+
+  7. Images
+  - 6. urls
+
+  9. Small Details
+  - horoscope
+  - smoking
+  - drinking
+  - children
+  - pets
+  - politics
+  - religion
+
+  */
+
 
 return (
 <section className="all-questions-container">
-
 <nav className="profile-nav-bar">
-  <a href="#question-1">age</a>
-  <a href="#question-2">location</a>
+  <a href="#question-1"><i className="fas fa-ice-cream"></i></a>
+  <a href="#question-2"><i class="fas fa-map-marker-alt"></i></a>
   <a href="#question-3">3</a>
   <a href="#question-4">4</a>
   <a href="#question-5">5</a>
   <a href="#question-6">6</a>
   <a href="#question-7">7</a>
-  <a href="#question-8">8</a>
+  <a href="#question-8">
+
+  <i class="fa-solid fa-ice-cream"></i>
+  </a>
   <a href="#question-9">9</a>
   <a href="#question-10">10</a>
 </nav>
 
     <section class="scroll-container">
 
-        <div class="scroll-question1-section" id="question-1">
-          <div className="question-big"> 1. Which word describes you better?</div>
-          <div className="vertical-line"></div>
-            <section className="question1ContainerE">
-                    <div className="questionTextE">Your Answer?</div>
-                    <div className ="question-choice">
-                      <RadioButton
-                          label="Carefree"
-                          value={question1 === 'Carefree'}
-                          onChange={(e) => setQuestion1("Carefree")}
-                        />
-                      <RadioButton
-                        label="Intense"
-                        value={question1 === 'Intense'}
-                        onChange={(e) => setQuestion1("Intense")}
-                      />
-                    </div>
 
-                    <div className="questionTextE">Answer you'll accept?</div>
-                    <div className ="question-choice">
-                      <RadioButton
-                          label="Carefree"
-                          value={must_answer1 === 'Carefree'}
-                          onChange={(e) => setMust_answer1("Carefree")}
-                        />
-                      <RadioButton
-                        label="Intense"
-                        value={must_answer1 === 'Intense'}
-                        onChange={(e) => setMust_answer1("Intense")}
-                      />
-                    </div>
-              </section>
-            <div className="horizontal-line"></div>
+        <div class="scroll-question1-section" id="question-1">
+          <div className="basic-detail-grid">
+
+            <div className="box1">
+              <div className="box1a">age</div>
+              <div className="box1b">height</div>
+            </div>
+            <div className="box2">pronouns</div>
+            <div className="box4">gender</div>
+            <div className="box5">orientation</div>
+
+          </div>
+
+          {/* <div className="vertical-line-profile"></div>
+
+          <div className="horizontal-line-profile"></div> */}
+
+
+
+
+
         </div>
 
 
