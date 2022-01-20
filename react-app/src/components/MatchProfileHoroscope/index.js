021184de0095d01727a1b233c74e55faf0297069
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-20 15:38:40 -0800

    add match percentage for horoscope on match profile

--- react-app/src/components/MatchProfileHoroscope/index.js
+++ react-app/src/components/MatchProfileHoroscope/index.js
@@ -30,14 +30,6 @@ console.log("horoscopeContent?.length",horoscopeContent?.length < 500 )
       <div className="matchContainerHeader">Horoscope Compatibility</div>
 
       <section className="MatchHoroscopeInnerContainer">
-        {/* <p className="matchHoroscopeContent">
-        <span className="bench">
-          <img src={horoscopePics.collection[12].imageUrl} />
-        </span>
-          {horoscopeContent}
-
-        </p> */}
-
         <p>
         <img className="bench" src={horoscopePics.collection[12].imageUrl} />
         <section className={horoscopeContent?.length < 500 ? 'contentHoroscopeFiller' : 'noContentHoroscopeFiller'}>
@@ -48,24 +40,18 @@ console.log("horoscopeContent?.length",horoscopeContent?.length < 500 )
         {horoscopeContent}
         </p>
 
-
-
         <div className="matchHoroscopeSign">
           <img src={horoscopePics.collection[matchProfile.horoscope_id - 1 ]?.sign} />
         </div>
         <div className="currentUserHoroscopeSign">
           <img src={horoscopePics.collection[currentUserHoroscopeId - 1 ]?.sign} />
         </div>
+        <div className="horoscopePercentBubble">
+        {getHoroscopeMatchPercent(matchProfile.horoscope_id, currentUserHoroscopeId)}%
+        </div>
+        {/* <div className="circle">&nbsp;</div> */}
       </section>
 
-{/* <p>
-<img className="bench" src={horoscopePics.collection[currentUserHoroscopeId - 1 ]?.sign} />
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue erosLorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velitLorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velitLorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras ac leo purus. Mauris quis diam velit.
-</p> */}
-
-
-
-            {getHoroscopeMatchPercent(matchProfile.horoscope_id, currentUserHoroscopeId)}
     </>
 
   )
