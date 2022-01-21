736d4a644cf053c7172ec0c218526a1bdf94120f
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-20 23:26:30 -0800

    add staircase for no search results

---
+++ react-app/src/components/SearchResults/NoSearchResults.js
@@ -0,0 +1,23 @@
+
+import './NoSearchResults.css'
+
+const NoSearchResults = ({input}) => {
+
+
+
+  return (
+
+    <section className="step-container-search">
+    <div className="Step1S">no</div>
+    <div className="Step2S">users</div>
+    <div className="Step3S">found</div>
+    <div className="Step5S">for</div>
+    <div className="Step6S">"{input}"</div>
+
+  </section>
+
+  )
+
+}
+
+export default NoSearchResults
