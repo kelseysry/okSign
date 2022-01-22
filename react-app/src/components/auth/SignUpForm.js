f4df0fe6366080b49426c391ae9bb3a3185cd4b9
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-21 16:21:33 -0800

    change sign up validation to on submit

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -13,36 +13,57 @@ const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-  const [frontErrors, setFrontErrors] = useState([])
+  // const [frontErrors, setFrontErrors] = useState([])
+  const [validationErrors, setValidationErrors] = useState([]);
 
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const validationErrors = []
+  // useEffect(() => {
+  //   const validationErrors = []
+
+  //   if(username.length < 4) {
+  //     validationErrors.push("username must be at least 4 characters")
+  //   } else if (username.length >= 20) {
+  //     validationErrors.push("username must be less than 20 characters")
+  //   }
+
+  //   if(password.length < 8 || password.length > 25) {
+  //     validationErrors.push("password must be 8-25 characters")
+  //   }
+  //   if(repeatPassword !== password) {
+  //     validationErrors.push("passwords must match!")
+  //   }
+
+  //   if(!isEmail(email)) {
+  //     validationErrors.push("please provide a valid email")
+  //   }
+
+  //   setFrontErrors(validationErrors)
+
+  // }, [username, password, repeatPassword, email])
+
+  const validate = () => {
+
+    const validationErrors = [];
 
     if(username.length < 4) {
-      validationErrors.push("username must be at least 4 characters")
+      validationErrors.push("Username must be at least 4 characters")
     } else if (username.length >= 20) {
-      validationErrors.push("username must be less than 20 characters")
+      validationErrors.push("Username must be less than 20 characters")
     }
-
     if(password.length < 8 || password.length > 25) {
-      validationErrors.push("password must be 8-25 characters")
+      validationErrors.push("Password must be 8-25 characters")
     }
     if(repeatPassword !== password) {
-      validationErrors.push("passwords must match!")
+      validationErrors.push("Passwords must match!")
     }
 
     if(!isEmail(email)) {
-      validationErrors.push("please provide a valid email")
+      validationErrors.push("Please provide a valid email")
     }
-
-    setFrontErrors(validationErrors)
-
-  }, [username, password, repeatPassword, email])
-
-
+    return validationErrors
+  }
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -50,9 +71,13 @@ const SignUpForm = () => {
       const data = await dispatch(signUp(first_name, last_name,username, email, password));
       if (data) {
         setErrors(data)
+        const frontErrors = validate();
+        if (frontErrors.length > 0) setValidationErrors(frontErrors);
+
       }
     }
   };
+
 
   const updateFirstName = (e) => {
     setFirstName(e.target.value);
@@ -82,13 +107,12 @@ const SignUpForm = () => {
     return <Redirect to='/' />;
   }
 
+  console.log("validationErrors",validationErrors)
+
   return (
     <div className="">
-
     <form className="sign-up-modal" onSubmit={onSignUp}>
-
       <div className="sign-up-spacer">
-        {/* <label>First Name</label> */}
         <input
           className="login-input"
           type='text'
@@ -99,7 +123,6 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className="sign-up-spacer">
-        {/* <label>Last Name</label> */}
         <input
           className="login-input"
           type='text'
@@ -110,7 +133,6 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className="sign-up-spacer">
-        {/* <label>User Name</label> */}
         <input
           className="login-input"
           type='text'
@@ -121,7 +143,6 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className="sign-up-spacer">
-        {/* <label>Email</label> */}
         <input
          className="login-input"
           type='text'
@@ -132,7 +153,6 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className="sign-up-spacer">
-        {/* <label>Password</label> */}
         <input
         className="login-input"
           type='password'
@@ -143,7 +163,6 @@ const SignUpForm = () => {
         ></input>
       </div>
       <div className="sign-up-spacer">
-        {/* <label>Repeat Password</label> */}
         <input
         className="login-input"
           type='password'
@@ -154,35 +173,25 @@ const SignUpForm = () => {
           required={true}
         ></input>
       </div>
-      {frontErrors.length?
+
+      {validationErrors.length || errors.length?
       <>
         <div className="errors-hr"></div>
         <ul className="error-signup">
-          {frontErrors.map((error) => <li key={error}>{error}</li>)}
+            {validationErrors.map((error) => <li key={error}>{error}</li>)}
+            {errors.map((error, idx) =>
+            <li key={idx}>{(error.split(':'))[1]}</li>
+            )}
         </ul>
       </>
       : null
-      }
-      {
-        errors.length?
-        <>
-      <div className="errors-hr"></div>
-
-      <ul className="error-signup">
-          {errors.map((error, idx) =>
-
-          <li key={idx}>{(error.split(':'))[1]}</li>
-
-          )}
-        </ul>
-        </>
-        : null
       }
 
       <div className="center-signup-button">
         <button className="login-button" type='submit'>Sign Up</button>
       </div>
     </form>
+
     </div>
   );
 };
